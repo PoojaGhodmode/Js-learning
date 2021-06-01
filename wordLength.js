acadb79b3e90020625d7d900c1e9dd76f150d2e5
poojaghodmode <poojaghodmode@gmail.com>
2021-06-01 22:28:51 +0530

    Add word length calculator

---
+++ wordLength.js
@@ -0,0 +1,7 @@
+const wordLengthCalculator = (word) => {
+  let wordLength = word.length;
+  return wordLength;
+};
+
+console.log('Word Length');
+console.log(wordLengthCalculator("this is string")); // 14
