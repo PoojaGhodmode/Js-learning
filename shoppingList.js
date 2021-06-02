18f3738e692c211f884cd769281f1495de70707b
poojaghodmode <poojaghodmode@gmail.com>
2021-06-02 20:15:37 +0530

    Add shopping list

---
+++ shoppingList.js
@@ -0,0 +1,25 @@
+var myItems = [
+  ["banana", 3],
+  ["juice", 2],
+  ["milk", 2],
+];
+
+const addItem = (items) => {
+  myItems.push(items);
+  console.log(`After adding item: ${myItems}.`);
+};
+
+const removeLastItem = () => {
+  myItems.pop();
+  console.log(`After removing last item: ${myItems}.`);
+};
+
+const getnthElement = (n = 1) => {
+  return myItems[n - 1];
+};
+
+addItem(["chocolate", 3]);
+removeLastItem();
+const n = 2;
+item = getnthElement(n);
+console.log(`${n}th item in list is ${item[0]}.`);
