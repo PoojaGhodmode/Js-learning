f47c392ec07c4a99b1208f6de88a49744861dd19
poojaghodmode <poojaghodmode@gmail.com>
2021-06-03 22:18:26 +0530

    Update bmi calculator(added levels)

--- bmiCalculator.js
+++ bmiCalculator.js
@@ -1,10 +1,28 @@
 const bmiCalculator = (weight, height) => {
-    let bmi;
-    bmi = weight / (height * height);
-    return bmi;
-  };
-  
-  console.log('BMI Calculator');
-  console.log(bmiCalculator(50, 1.55)); 
-  //20.811654526534856
-  
+  let bmi;
+  bmi = weight / (height * height);
+  let status;
+  if (bmi < 20.7) {
+    status = "Underweight";
+  } else if (bmi >= 20.7 && bmi < 26.4) {
+    status = "Normal";
+  } else if (bmi >= 26.4 && bmi < 27.8) {
+    status = "Marginally Overweight";
+  } else if (bmi >= 27.8 && bmi < 31.1) {
+    status = "Overweight";
+  } else {
+    status = "Obses";
+  }
+  return  {status,bmi};
+};
+
+console.log("BMI Calculator");
+let output = bmiCalculator(50, 1.55);
+console.log(`${output.bmi} => ${output.status}`);
+//20.811654526534856 => Normal
+output = bmiCalculator(40, 1.55);
+console.log(`${output.bmi} => ${output.status}`);
+//16.649323621227886 => Underweight
+output = bmiCalculator(70, 1.55);
+console.log(`${output.bmi} => ${output.status}`);
+//29.1363163371488 => Overweight
