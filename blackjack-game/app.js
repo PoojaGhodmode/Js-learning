4c71507164d5af3c865a9d5151e4f6a7e8189364
poojaghodmode <poojaghodmode@gmail.com>
2021-06-27 20:33:29 +0530

    Complete Blackjack

--- blackjack-game/app.js
+++ blackjack-game/app.js
@@ -1,57 +1,79 @@
 const messageBox = document.querySelector(".message");
 const sumBox = document.querySelector(".sum span");
 const cards = document.querySelector(".cards span");
-const newCardbtn = document.querySelector(".new-card")
-let firstCard;
-let secondCard;
-let cardSArray = [firstCard, secondCard]
-let sumCard;
+const newCardbtn = document.querySelector(".new-card");
+const player = document.querySelector(".player-chips");
+let name = prompt("Enter your name");
+
+const playerDetails = {
+  name: name,
+  chips: 150,
+  decreaseChips() {
+    this.chips -= 30;
+  },
+  increaseChips() {
+    this.chips += 50;
+  },
+  showDetails() {
+    player.textContent = `${this.name}: $${this.chips}`;
+  },
+};
+
+let cardSArray = [];
 let isGameOver;
 
 const drawCard = () => {
-  return Math.ceil(Math.random() * 12);
+  cardSArray.push(Math.ceil(Math.random() * 12));
 };
 
-const sum = (first, second) => {
-  return first + second;
-};
+playerDetails.showDetails();
 
-const Game = (sumCard) => {
-  //   console.log(sumCard);
+const Game = () => {
+  let sumCard = 0;
+  cards.textContent = "";
+  cardSArray.forEach((card) => {
+    sumCard += card;
+    console.log(sumCard);
+    cards.textContent += `${card} `;
+  });
+  // cards.textContent = `${firstCard} ${secondCard}`;
+
   let message = "";
   if (sumCard === 21) {
     message = "You win";
     isGameOver = true;
+    playerDetails.increaseChips();
   } else if (sumCard < 21) {
     message = "Do you want to draw new card?";
     isGameOver = false;
   } else {
     message = "You Lose";
     isGameOver = true;
+    playerDetails.decreaseChips();
   }
   console.log(message);
   messageBox.textContent = message;
   sumBox.textContent = sumCard;
+  playerDetails.showDetails();
 };
 
 const startGame = () => {
-  newCardbtn.classList.remove('disabled')
-  firstCard = drawCard();
-  secondCard = drawCard();
-  sumCard = firstCard + secondCard;
-  cards.textContent = `${firstCard} ${secondCard}`;
-  Game(sumCard);
+  if (playerDetails.chips >= 30) {
+    cardSArray = [];
+    newCardbtn.classList.remove("disabled");
+    drawCard();
+    drawCard();
+    Game();
+  } else if (playerDetails.chips < 30) {
+    document.querySelector(".start-game").classList.add("disabled");
+    messageBox.textContent = "Its time for a break!!!";
+  }
 };
 
 const drawNewCard = () => {
-  let newCard = drawCard();
-  sumCard += newCard;
-  cards.textContent += ` ${newCard}`;
-  sumBox.textContent = sumCard;
-  console.log(sumCard);
-  Game(sumCard);
-  if (isGameOver){
-    // messageBox.textContent += "...want to start new game";
-    newCardbtn.classList.add('disabled')
+  drawCard();
+  Game();
+  if (isGameOver) {
+    newCardbtn.classList.add("disabled");
   }
 };
