bc5a7308a5784e9e9974967b72c26b6437a1e0b8
poojaghodmode <poojaghodmode@gmail.com>
2021-07-20 12:57:25 +0530

    Add readyState and status code

---
+++ Day17/app.js
@@ -0,0 +1,25 @@
+const request = new XMLHttpRequest();
+
+request.addEventListener("readystatechange", () => {
+  //   console.log(request.response, request.readyState);
+  //readyState
+  //0 - UNSEND [not called open() yet]
+  //1 - Opened [open is called]
+  //2 - header received [send() called and header and status is received]
+  //3 - Loading [responseText holds partial data]
+  //4 - Done [operation is complete]
+
+  if (request.readyState === 4 && request.status === 200) {
+    //10X - ABout information
+    //20X - Success 200-OK
+    //30X - Redirection messages
+    //40X - Client error message 404-NOT FOUND
+    //50X - server error message
+    console.log(request, request.responseText);
+  } else if (request.readyState === 4) {
+    console.log("could not fetch the data");
+  }
+});
+request.open("GET", "https://jsonplaceholder.typicode.com/todos/1"); // readyState - 1
+
+request.send(); // readyState - 2
