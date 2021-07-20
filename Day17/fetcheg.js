20c82f7a2199f49589521fd668f2031639317364
poojaghodmode <poojaghodmode@gmail.com>
2021-07-20 20:45:44 +0530

    Add async and await example

--- Day17/fetcheg.js
+++ Day17/fetcheg.js
@@ -1,15 +1,33 @@
 // Fetch api
 // fetch only throw error when there is network issue
 
-fetch("todos/part1.json")
-  .then((response) => {
-    console.log(response);
-    //return response.text() return promise with data type of string
-    return response.json();
-     //return response.json() return promise with data type of object
-  })
+// fetch("todos/part1.json")
+//   .then((response) => {
+//     console.log(response);
+//     //return response.text() return promise with data type of string
+//     return response.json();
+//     //return response.json() return promise with data type of object
+//   })
+//   .then((data) => {
+//     console.log(data, typeof data);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+// async await
+
+// async always returns a promise
+const getTodos = async () => {
+  const response = await fetch("todos/part1.json");
+  // await => once promise is resolved it assign value to variable response
+  const data = await response.json();
+  return data;
+};
+
+getTodos()
   .then((data) => {
-    console.log(data, typeof data);
+    console.log(data);
   })
   .catch((err) => {
     console.log(err);
